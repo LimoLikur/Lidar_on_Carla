6cb9a7a45d3f83782a13190c9a09a90399d12a38
LimoLikur <yanuarfiqri7@gmail.com>
2025-07-06 05:34:41 +0000

    5th commit dari Google Colab

--- scripts/lidar99.py
+++ scripts/lidar99.py
@@ -8,9 +8,10 @@ latest_points = None
 
 def lidar_callback(point_cloud):
     global latest_points
-    # Data: x, y, z, intensity
     points = np.frombuffer(point_cloud.raw_data, dtype=np.float32).reshape(-1, 4)[:, :3]
     print("Jumlah titik lidar:", points.shape[0])
+    if points.shape[0] > 0:
+        print("Contoh point:", points[0])
     latest_points = points
 
 def main():
@@ -21,21 +22,19 @@ def main():
     blueprint_library = world.get_blueprint_library()
     vehicle_bp = blueprint_library.filter('vehicle')[0]
     spawn_points = world.get_map().get_spawn_points()
-    # Pilih spawn point random agar dekat objek
     spawn_point = random.choice(spawn_points)
     vehicle = world.try_spawn_actor(vehicle_bp, spawn_point)
 
     lidar_bp = blueprint_library.find('sensor.lidar.ray_cast')
-    lidar_bp.set_attribute('channels', '32')
-    lidar_bp.set_attribute('points_per_second', '56000')
+    lidar_bp.set_attribute('channels', '64')
+    lidar_bp.set_attribute('points_per_second', '130000')
     lidar_bp.set_attribute('rotation_frequency', '10')
-    lidar_bp.set_attribute('range', '50')
-    lidar_transform = carla.Transform(carla.Location(x=0, z=2.5))
+    lidar_bp.set_attribute('range', '100')
+    lidar_transform = carla.Transform(carla.Location(x=0, y=0, z=2.5))
     lidar_sensor = world.spawn_actor(lidar_bp, lidar_transform, attach_to=vehicle)
 
     lidar_sensor.listen(lidar_callback)
 
-    # Tunggu sampai data masuk
     print("Menunggu data Lidar...")
     while latest_points is None or len(latest_points) == 0:
         time.sleep(0.1)
@@ -44,7 +43,7 @@ def main():
     vis = o3d.visualization.Visualizer()
     vis.create_window(window_name="Lidar Point Cloud")
     render_option = vis.get_render_option()
-    render_option.point_size = 7.0  # BESAR biar kelihatan
+    render_option.point_size = 7.0
     pcd = o3d.geometry.PointCloud()
     vis.add_geometry(pcd)
 
@@ -52,6 +51,7 @@ def main():
         while True:
             if latest_points is not None and len(latest_points) > 0:
                 pcd.points = o3d.utility.Vector3dVector(latest_points)
+                pcd.colors = o3d.utility.Vector3dVector(np.tile([0, 0, 1], (latest_points.shape[0], 1)))  # warna biru
                 vis.update_geometry(pcd)
                 vis.poll_events()
                 vis.update_renderer()
