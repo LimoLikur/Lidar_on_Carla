6c6e099d6d12b59f62539bf70637b2e650afffc7
LimoLikur <yanuarfiqri7@gmail.com>
2025-07-06 05:10:00 +0000

    5th commit dari Google Colab

--- scripts/lidar2.py
+++ scripts/lidar2.py
@@ -5,29 +5,36 @@ import cv2
 import threading
 import time
 
-# Variabel global untuk kamera dan lidar
+# Ukuran window
+WINDOW_W, WINDOW_H = 400, 300
+
+# Variabel global
 latest_image = None
 latest_points = None
 stop_threads = False
 
 def camera_callback(image):
     global latest_image
-    # Convert ke array numpy
+    # Convert ke numpy array, reshape, dan resize
     img = np.frombuffer(image.raw_data, dtype=np.uint8)
     img = img.reshape((image.height, image.width, 4))
     img = img[:, :, :3]  # Buang alpha
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    img = cv2.resize(img, (WINDOW_W, WINDOW_H))
     latest_image = img
 
 def lidar_callback(point_cloud):
     global latest_points
-    points = np.frombuffer(point_cloud.raw_data, dtype=np.float32).reshape(-1, 4)[:, :3]
-    latest_points = points
+    # Ambil x, y, z, intensity
+    points = np.frombuffer(point_cloud.raw_data, dtype=np.float32).reshape(-1, 4)
+    latest_points = points  # (N, 4): x, y, z, intensity
 
 def camera_window():
     global stop_threads
+    cv2.namedWindow("Kamera Mobil", cv2.WINDOW_AUTOSIZE)
     while not stop_threads:
         if latest_image is not None:
-            cv2.imshow("Kamera Depan Mobil", latest_image)
+            cv2.imshow("Kamera Mobil", latest_image)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
@@ -37,16 +44,23 @@ def camera_window():
 def lidar_window():
     global stop_threads
     vis = o3d.visualization.Visualizer()
-    vis.create_window(window_name="Lidar Point Cloud")
+    vis.create_window(window_name="Lidar Point Cloud", width=WINDOW_W, height=WINDOW_H, left=50, top=50)
     pcd = o3d.geometry.PointCloud()
     vis.add_geometry(pcd)
     while not stop_threads:
         if latest_points is not None:
-            pcd.points = o3d.utility.Vector3dVector(latest_points)
+            xyz = latest_points[:, :3]
+            intens = latest_points[:, 3]
+            # Normalisasi intensitas ke [0, 1], pewarnaan gradasi (cmap jet)
+            intens_norm = (intens - intens.min()) / (intens.ptp() + 1e-6)
+            colors = cv2.applyColorMap(np.clip((intens_norm*255).astype(np.uint8),0,255), cv2.COLORMAP_JET)
+            colors = colors[:, :3][:, ::-1] / 255.0  # BGR to RGB
+            pcd.points = o3d.utility.Vector3dVector(xyz)
+            pcd.colors = o3d.utility.Vector3dVector(colors)
             vis.update_geometry(pcd)
             vis.poll_events()
             vis.update_renderer()
-            time.sleep(0.05)
+            time.sleep(0.02)
         else:
             time.sleep(0.05)
     vis.destroy_window()
@@ -61,14 +75,14 @@ def main():
     spawn_point = world.get_map().get_spawn_points()[0]
     vehicle = world.try_spawn_actor(vehicle_bp, spawn_point)
 
-    # Setup kamera RGB
+    # Kamera
     camera_bp = blueprint_library.find('sensor.camera.rgb')
     camera_bp.set_attribute('image_size_x', '800')
     camera_bp.set_attribute('image_size_y', '600')
     camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
     camera = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
 
-    # Setup lidar
+    # Lidar
     lidar_bp = blueprint_library.find('sensor.lidar.ray_cast')
     lidar_bp.set_attribute('channels', '32')
     lidar_bp.set_attribute('points_per_second', '56000')
@@ -81,7 +95,6 @@ def main():
     camera.listen(camera_callback)
     lidar_sensor.listen(lidar_callback)
 
-    # Mulai 2 thread window
     t1 = threading.Thread(target=camera_window)
     t2 = threading.Thread(target=lidar_window)
     t1.start()
